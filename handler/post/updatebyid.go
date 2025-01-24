17a2c3defacbf86b8e1c3d22932595df825f1feb
Alex Wu <alexwu21215@gmail.com>
2025-01-24 19:25:00 +0800

    updatebyid

--- handler/post/updatebyid.go
+++ handler/post/updatebyid.go
@@ -6,5 +6,5 @@ import (
 
 func (h *Post) updatebyid(c *gin.Context) {
 	id := c.Param("id")
-	post, err := h.postService.UpdateOne(map[string]interface{}{"id": id}, map[string]interface{}{"title": id})
+	post, err := h.postService.UpdateOne(map[string]interface{}{"id": id}, map[string]interface{}{"title": c.Param("title")})
 }
