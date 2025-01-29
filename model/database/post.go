a844915d2de1acb632ba92f32ef92226da7e21ad
Alex Wu <alexwu21215@gmail.com>
2025-01-30 01:15:28 +0800

    feat:Makefile add create student

--- model/database/post.go
+++ model/database/post.go
@@ -2,16 +2,18 @@ package database
 
 import (
 	"devmentor-BE103-golang/infrastructure"
-	"gorm.io/gorm"
 	"time"
+
+	"gorm.io/gorm"
 )
 
 // Post model
 type Post struct {
-	Id        int       `json:"id",gorm:"primarykey"`
-	Title     string    `json:"title",gorm:"column:title"`
-	Content   string    `json:"content",gorm:"column:content"`
-	CreatedAt time.Time `json:"created_at",gorm:"column:created_at"`
+	Id        int       `json:"id" gorm:"primarykey"`
+	Title     string    `json:"title" gorm:"column:title"`
+	Content   string    `json:"content" gorm:"column:content"`
+	CreatedAt time.Time `json:"created_at" gorm:"column:created_at"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"column:updated_at"`
 }
 
 func (Post) TableName() string {
